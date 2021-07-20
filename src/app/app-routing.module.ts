c043828f732f951fbf973ae627ed326e676ccafa
[Amaan09] <[mohammedamaan79@gmail.com]>
2021-07-20 11:22:19 +0530

    Added Home component and responsiveness

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/' },
   { path: '', component: HomeComponent},
   { path: 'aboutme', loadChildren: () => import('./about-me/about-me.module').then(m => m.AboutMeModule) },
   { path: '**', redirectTo: '/' }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
@@ -14,6 +14,8 @@ import { HeaderComponent } from './shared/components/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+
 import { LoaderComponent } from './shared/components/loader/loader.component';
 import { InMemoryDataService } from './services/in-memory-data/in-memory-data.service';
 import { httpInterceptorProviders } from './interceptors/index';
@@ -32,6 +34,7 @@ import { HomeComponent } from './home/home.component';
     ToastrModule.forRoot(),
     MatToolbarModule,
     MatProgressSpinnerModule,
+    MatButtonModule,
     FlexLayoutModule,
   ],
   providers: [
